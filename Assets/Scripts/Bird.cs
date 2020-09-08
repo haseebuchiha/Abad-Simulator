4783ff6319439572785f17ccf1fc0252999c7e60
haseebuchiha <haseeb9009@gmail.com>
2020-09-08 06:44:51 +0500

    Initial COmmit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public float upForce = 200f;
+
+    private bool bIsDead = false;
+
+    private Rigidbody2D rb2d;
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!bIsDead)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                animator.SetTrigger("Flap");
+            }
+        }
+	}
+
+    private void OnCollisionEnter2D()
+    {
+        animator.SetTrigger("Die");
+        rb2d.velocity = Vector2.zero;
+        bIsDead = true;
+        GameControl.instance.BirdDied();
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl instance;
+
+    public GameObject gameOverText;
+
+    public bool gameOver = false;
+
+    public float scrollSpeed = -10.5f;
+
+    private int score = 0;
+
+    public Text scoreText;
+
+    public AudioClip deathSound;
+
+    public AudioClip normalSound;
+
+    public AudioSource MusicSource;
+
+	// Use this for initialization
+	void Awake () {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        MusicSource.clip = normalSound;
+        MusicSource.Play();
+    }
+
+   
+
+    // Update is called once per frame
+    void Update () {
+        if (gameOver && Input.GetMouseButtonDown(0))//|| Input.touchCount > 0) 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Year: " + score.ToString();
+    }
+
+    public void BirdDied()
+    {
+        MusicSource.Stop();
+        MusicSource.clip = deathSound;
+        MusicSource.Play();
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+
+}
